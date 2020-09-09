7c01f7939cfea2d39247ee6a3b2948e4aeaafc9c
Deepakmalempati <deepakmalempati@gmail.com>
2020-09-09 14:00:23 -0500

    search bar included

---
+++ Big-Kart/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBqOrei74F8bc_gx5Da4ObEoJHZUNiIozA",
+    authDomain: "bigkart-bdb25.firebaseapp.com",
+    databaseURL: "https://bigkart-bdb25.firebaseio.com",
+    projectId: "bigkart-bdb25",
+    storageBucket: "bigkart-bdb25.appspot.com",
+    messagingSenderId: "1054476211210",
+    appId: "1:1054476211210:web:ffc53449434e4ca929fbf3",
+    measurementId: "G-962J9KN38Y"
+  }
+};
---
+++ Big-Kart/src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBqOrei74F8bc_gx5Da4ObEoJHZUNiIozA",
+    authDomain: "bigkart-bdb25.firebaseapp.com",
+    databaseURL: "https://bigkart-bdb25.firebaseio.com",
+    projectId: "bigkart-bdb25",
+    storageBucket: "bigkart-bdb25.appspot.com",
+    messagingSenderId: "1054476211210",
+    appId: "1:1054476211210:web:ffc53449434e4ca929fbf3",
+    measurementId: "G-962J9KN38Y"   
+  }
+};
