d95a8019d32f10fecb312752b37b7b098e7829fa
Gangadhar <54452212+gangadhary574@users.noreply.github.com>
2020-09-11 23:34:32 -0500

    added sign-in/ sign-up/ forgot-password components

--- Big-Kart/src/app/app.module.ts
+++ Big-Kart/src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { AngularFireModule } from 'angularfire2';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { AngularFireModule } from "angularfire2";
 
-import { environment } from './../environments/environment';
-import { AdminModule } from './admin/admin.module';
-import { AdminAuthGuard } from './admin/services/admin-auth-guard.service';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './core/components/login/login.component';
-import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
-import { ProductsComponent } from './shopping/components/products/products.component';
-import { ShoppingModule } from './shopping/shopping.module';
+import { environment } from "./../environments/environment";
+import { AdminModule } from "./admin/admin.module";
+import { AdminAuthGuard } from "./admin/services/admin-auth-guard.service";
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./core/components/login/login.component";
+import { UserSignUpComponent } from "./core/components/user-sign-up/user-sign-up.component";
+import {ForgotPasswordComponent} from "./core/components/forgot-password/forgot-password.component";
+import { CoreModule } from "./core/core.module";
+import { SharedModule } from "./shared/shared.module";
+import { ProductsComponent } from "./shopping/components/products/products.component";
+import { ShoppingModule } from "./shopping/shopping.module";
 
 @NgModule({
-  declarations: [
-    AppComponent   
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     SharedModule,
@@ -25,13 +25,13 @@ import { ShoppingModule } from './shopping/shopping.module';
     CoreModule,
     AngularFireModule.initializeApp(environment.firebase),
     RouterModule.forRoot([
-      { path: '', component: ProductsComponent },
-      { path: 'login', component: LoginComponent },
-    ])    
+      { path: "", component: ProductsComponent },
+      { path: "login", component: LoginComponent },
+      { path: "signUp", component: UserSignUpComponent },
+      { path: "forgotPassword", component:ForgotPasswordComponent }
+    ]),
   ],
-  providers: [
-    AdminAuthGuard,
-  ],
-  bootstrap: [AppComponent]
+  providers: [AdminAuthGuard],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
